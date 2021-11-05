cce8e4b3640543fc9f82c7d385d6fdf0544b3214
Afloatz <florentlaot.fr@gmail.com>
2021-11-05 14:49:04 +0000

    base template for header and navbar

--- src/main/java/com/webapp/craictivity/WebSecurityConfiguration.java
+++ src/main/java/com/webapp/craictivity/WebSecurityConfiguration.java
@@ -38,6 +38,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
     }
 }
